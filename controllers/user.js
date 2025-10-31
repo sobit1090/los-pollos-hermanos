a1917be4de937077befdeabf15042d450d0fc93f
sobit sobit <112492551+sobit1090@users.noreply.github.com>
2025-10-31 13:49:45 +0530

    Update user.js added the chnages in logout fn

--- controllers/user.js
+++ controllers/user.js
@@ -12,16 +12,23 @@ export const myProfile = (req, res, next) => {
 export const logout = (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
+
     res.clearCookie("connect.sid", {
-      secure: process.env.NODE_ENV === "development" ? false : true,
+      path: "/",
       httpOnly: process.env.NODE_ENV === "development" ? false : true,
+      secure: process.env.NODE_ENV === "development" ? false : true,
       sameSite: process.env.NODE_ENV === "development" ? false : "none",
     });
-    res.status(200).json({
-      message: "Logged Out",
-    });
+
+    // ✅ Redirect to frontend after logout
+    return res.redirect(
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5173/login"
+        : process.env.CLIENT_URL + "/login"
+    );
   });
 };
+
 
 export const getAdminUsers = asyncError(async (req, res, next) => {
   const users = await User.find({});
